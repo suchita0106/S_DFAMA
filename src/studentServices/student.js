28b5f740b85c0f98b2e298f76c7441310ad7d2e0
absolution54321 <shrutarshis8@gmail.com>
2017-07-23 20:49:56 +0530

    student agenda fetching

--- src/studentServices/student.js
+++ src/studentServices/student.js
@@ -1,30 +1,28 @@
 var mysql = require("mysql");
 
 var config = {
-    "host": "127.0.0.1",
-     "user": "dev",
+    "host": "localhost",
+    "user": "dev",
     "password": "dev",
     "database": "project"
 };
 
 
 module.exports =
-{
-    // Login Validator
-    "validateStudent": function(callback, inputData)
-        {
+    {
+        // Login Validator
+        "validateStudent": function (callback, inputData) {
             var connection = mysql.createConnection(config);
             connection.connect();
 
             var sql = "SELECT * FROM student where studentUsername=? && studentPassword=?";
             var param = [inputData.UserId, inputData.UserPwd];
-                
-            connection.query(sql, param, function(err, results) {
-               if(!err)
-                {
+
+            connection.query(sql, param, function (err, results) {
+                if (!err) {
                     console.log(results);
-                } 
-                
+                }
+
 
                 // TASK COMPLETED
                 callback(err, results)
@@ -33,23 +31,40 @@ module.exports =
         },
 
         // If Login Successful get Mentor Details
-    "fetchStudent": function(callback, inputData)
-        {
+        "fetchStudent": function (callback, inputData) {
             var connection = mysql.createConnection(config);
             connection.connect();
 
             var sql = "SELECT * FROM studentdetails where studentId=?";
             var param = [inputData.studentId];
-                
-            connection.query(sql, param, function(err, results) {
-               if(!err)
-                {
+
+            connection.query(sql, param, function (err, results) {
+                if (!err) {
                     console.log(results);
-                } 
+                }
 
                 // TASK COMPLETED
                 callback(err, results)
                 connection.end();
             });
+        },
+
+        "fetchAgenda": function (callback, inputData) {
+            var connection = mysql.createConnection(config);
+            connection.connect();
+
+            var sql = "select agenda_Details, scheduleTime from agenda where mentorId = (select mentorId from studentdetails where studentId = ?)";
+            // var sql = ADD YOUR QUERRY HERE AND COMMENT THE ABOVE
+            var param = [inputData.studentId];
+
+            connection.query(sql, param, function (err, results) {
+                if (!err) {
+                    console.log(results);
+                }
+
+                callback(err, results)
+                connection.end();
+            });
+
         }
-}
+    }
